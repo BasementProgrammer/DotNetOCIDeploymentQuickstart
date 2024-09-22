df118ea010676526c0c4ac83b73eb93cea658352
Tom Moore <tom@basementprogrammer.com>
2024-09-22 19:41:18 -0400

    Updated to create two instances

--- tf_project/linux_compute.tf
+++ tf_project/linux_compute.tf
@@ -27,7 +27,7 @@
           ad_number      = 1
       }
   
-      resource "oci_core_instance" "this" {
+      resource "oci_core_instance" "App_Instance_1" {
         # availability_domain  = data.oci_core_subnet.this.availability_domain
         availability_domain  = "${data.oci_core_subnet.this.availability_domain != null ? data.oci_core_subnet.this.availability_domain : data.oci_identity_availability_domain.ad.name}"
         compartment_id       = var.compartment_ocid
@@ -41,11 +41,9 @@
             ocpus = "2"
         }
         create_vnic_details {
-          #assign_public_ip       = var.assign_public_ip
           assign_public_ip       = true
           display_name           = var.vnic_name
-          hostname_label         = var.hostname_label
-          #private_ip             = var.private_ip
+          hostname_label         = "AppInstance1"
           skip_source_dest_check = var.skip_source_dest_check
           subnet_id              = var.subnet_ocid
         }
@@ -67,18 +65,71 @@
         }
       }
 
-      resource "oci_core_volume" "this" {
-        availability_domain = oci_core_instance.this.availability_domain
+      resource "oci_core_volume" "App_Volume_1" {
+        availability_domain = oci_core_instance.App_Instance_1.availability_domain
         compartment_id      = var.compartment_ocid
-        display_name        = "${oci_core_instance.this.display_name}_volume_0"
+        display_name        = "${oci_core_instance.App_Instance_1.display_name}_volume_0"
         size_in_gbs         = var.block_storage_size_in_gbs
       }
 
-      resource "oci_core_volume_attachment" "this" {
+      resource "oci_core_volume_attachment" "App_Volume_Attachment_1" {
         attachment_type = var.attachment_type
         # compartment_id  = var.compartment_ocid
-        instance_id     = oci_core_instance.this.id
-        volume_id       = oci_core_volume.this.id
+        instance_id     = oci_core_instance.App_Instance_1.id
+        volume_id       = oci_core_volume.App_Volume_1.id
+        use_chap        = var.use_chap
+      }
+
+      resource "oci_core_instance" "App_Instance_2" {
+        # availability_domain  = data.oci_core_subnet.this.availability_domain
+        availability_domain  = "${data.oci_core_subnet.this.availability_domain != null ? data.oci_core_subnet.this.availability_domain : data.oci_identity_availability_domain.ad.name}"
+        compartment_id       = var.compartment_ocid
+        display_name         = var.instance_display_name
+        ipxe_script          = var.ipxe_script
+        preserve_boot_volume = var.preserve_boot_volume
+        shape                = var.shape
+        
+        shape_config {
+            memory_in_gbs = "32"
+            ocpus = "2"
+        }
+        create_vnic_details {
+          assign_public_ip       = true
+          display_name           = var.vnic_name
+          hostname_label         = "AppInstance2"
+          skip_source_dest_check = var.skip_source_dest_check
+          subnet_id              = var.subnet_ocid
+        }
+
+        #ssh_authorized_keys = var.ssh_public_key
+        metadata = {
+          ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCboQCXc8GKbaazjznwIgOofSyax/yjLp83RYMvKyb4AZ6kujjKQmUg+TsYYNpVcUNPBBisbJ29oCBdmSsAeBWD0Hs/GpiaD5iu9lgv6UfFC5/OAbsIeeRLxn16Q0yanqBW/ZI4ghruUVRUWWDKNqOqPp1nyud3S56yFsGkxbpIndsyRi4o5l07RaG6QpjXoSl/ndO0feALr5WaOOJjlx/PbQTEmBUagL23q8sbdBbs60Ql6CiF8hlYr1Am65hmNETBMcY2NRcnd1ctRj/faOiELCFU7qf2h/iLQApUEC1q3FUe5mSdKmtDgux4nTXs8To9+7cEAKSiq7VpXBeBm0m9 tom_m_moor@7f80a06c282b"
+          user_data           = "${base64encode(file("./app_install.sh"))}"
+        }
+
+        source_details {
+          boot_volume_size_in_gbs = var.boot_volume_size_in_gbs
+          source_type = "image"
+          source_id   = data.oci_core_images.this.images[0].id
+        }
+
+        timeouts {
+          create = var.instance_timeout
+        }
+      }
+
+      resource "oci_core_volume" "App_Volume_2" {
+        availability_domain = oci_core_instance.App_Instance_2.availability_domain
+        compartment_id      = var.compartment_ocid
+        display_name        = "${oci_core_instance.App_Instance_2.display_name}_volume_0"
+        size_in_gbs         = var.block_storage_size_in_gbs
+      }
+
+      resource "oci_core_volume_attachment" "App_Volume_Attachment_2" {
+        attachment_type = var.attachment_type
+        # compartment_id  = var.compartment_ocid
+        instance_id     = oci_core_instance.App_Instance_2.id
+        volume_id       = oci_core_volume.App_Volume_2.id
         use_chap        = var.use_chap
       }
     
--- tf_project/outputs.tf
+++ tf_project/outputs.tf
@@ -2,18 +2,34 @@
     // Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
     // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
     
-      output "instance_id" {
-        description = "ocid of created instances. "
-        value       = [oci_core_instance.this.id]
+      output "instance_id_1" {
+        description = "ocid of App Instance 1. "
+        value       = [oci_core_instance.App_Instance_1.id]
       }
       
-      output "private_ip" {
-        description = "Private IPs of created instances. "
-        value       = [oci_core_instance.this.private_ip]
+      output "private_ip_1" {
+        description = "Private IPs App Instance 1. "
+        value       = [oci_core_instance.App_Instance_1.private_ip]
       }
       
-      output "public_ip" {
-        description = "Public IPs of created instances. "
-        value       = [oci_core_instance.this.public_ip]
+      output "public_ip_1" {
+        description = "Public IPs of App Instance 1. "
+        value       = [oci_core_instance.App_Instance_1.public_ip]
+      }
+
+
+      output "instance_id_2" {
+        description = "ocid of App Instance 2. "
+        value       = [oci_core_instance.App_Instance_2.id]
+      }
+      
+      output "private_ip_2" {
+        description = "Private IPs App Instance 2. "
+        value       = [oci_core_instance.App_Instance_2.private_ip]
+      }
+      
+      output "public_ip_2" {
+        description = "Public IPs of App Instance 2. "
+        value       = [oci_core_instance.App_Instance_2.public_ip]
       }
   
