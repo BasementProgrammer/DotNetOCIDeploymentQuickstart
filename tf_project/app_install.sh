b614b98b1d098ef54171dce8cd3c060ab17617a6
Tom Moore <tom@basementprogrammer.com>
2024-09-21 20:13:52 -0400

    CHange the OCI CLI DOwnload to a WGET to simplify the install.

--- tf_project/app_install.sh
+++ tf_project/app_install.sh
@@ -38,7 +38,8 @@ echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
 # Install the OCL-CLI
 apt install -y python3-oci-cli
 # Download the application from your bucket
-oci os object get -bn install-files --name App.zip --file ./App.zip --auth instance_principal
+# oci os object get -bn install-files --name App.zip --file ./App.zip --auth instance_principal
+wget https://github.com/BasementProgrammer/DotNetOCIDeploymentQuickstart/blob/main/SampleApplication/App.zip -O App.zip
 unzip ./App.zip -d /var/www/App
 
 # Create Kestral service for the asp.net application
